c6e6ba066c7c6abb2670f6aac2c4b975b8895732
Hyunmin.Cho <71583831+Hyunmin-jasper-Cho@users.noreply.github.com>
2022-08-03 11:04:36 +0900

    Update demo.sh

--- Python/demo.sh
+++ Python/demo.sh
@@ -1,5 +1,17 @@
 # You should set corresponding width and height of each .YUV file
 
+#  ########################################
+#  ##       YUV2PNG-python3 runner       ##
+#  ##                                    ##
+#  ##     you shoud send 6 parameters    ##
+#  ##      1. YUV root path (./YUVs)     ##
+#  ##      2. IMG root path (./PNGs)     ##
+#  ##            3. Bit depth            ##
+#  ##              4. width              ##
+#  ##              5. height             ##
+#  ##        6. format (lower case)      ##
+#  ########################################
+
 # 1. To get 10bit .png file from 4:2:0 10bit YUV
 python3 yuv2png.py ./YUV420_10 ./TIFF_10 10 3840 2176 tiff
 
